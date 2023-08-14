a50994d6ff69199f0bcd8ca06069239390b15488
Pravesh <praveshjha5@gmail.com>
2023-08-14 19:25:48 +0530

    password recovery and reset UI

---
+++ app/auth/recoverPasswordLink/page.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import React from 'react'
+import { AbsoluteCenter, Box, Button, Text, Center, Divider, Input, Link, Stack } from '../../chakraExports'
+import { FcGoogle } from 'react-icons/fc'
+
+function recoverPassword() {
+
+  const goBack = () => {
+    console.log("go back")
+  }
+
+  return (
+    <Stack spacing={5}>
+      <Text fontSize='lg' color="white">Enter the email associated with your account</Text>
+      <Input type='email' textColor='antiquewhite' placeholder='Email' size='lg' />
+      <Button mt={7} colorScheme='messenger' size='lg'>Continue</Button>
+      <Center mt={3}><button onClick={goBack}><Text fontSize='lg' color="messenger.300">Go Back</Text></button></Center>
+    </Stack>
+  )
+}
+
+export default recoverPassword

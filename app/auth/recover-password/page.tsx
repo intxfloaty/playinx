50d5ae512b0625fc191d2be6c305dded15772144
Pravesh <praveshjha5@gmail.com>
2023-08-16 23:13:40 +0530

    so that user can reset password

--- app/auth/recover-password/page.tsx
+++ app/auth/recover-password/page.tsx
@@ -1,23 +1,96 @@
-"use client"
-
-import React from 'react'
-import { AbsoluteCenter, Box, Button, Text, Center, Divider, Input, Link, Stack } from '../../chakraExports'
-import { FcGoogle } from 'react-icons/fc'
+"use client";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Text,
+  Center,
+  Input,
+  Stack,
+} from "../../chakraExports";
+import supabase from "../../../src/utils/supabase";
+import Link from "next/link";
 
 function recoverPassword() {
+  const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [isResetPasswordLink, setIsPasswordLink] = useState(false);
 
-  const goBack = () => {
-    console.log("go back")
-  }
+  const validate = () => {
+    let error = "";
+    if (!email) {
+      error = "Please enter your email address";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
+      error = "Invalid email address";
+    }
+    return error;
+  };
+
+  const handleContinueClick = async () => {
+    const error = validate();
+    setEmailError(error);
+    if (!error) {
+      try {
+        let { data, error } = await supabase.auth.resetPasswordForEmail(email, {
+          redirectTo: "http://localhost:3000/auth/reset-password",
+        });
+        console.log(data, "data");
+        console.log(error, "error");
+        if (error === null) {
+          setIsPasswordLink(true);
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (emailError !== "") {
+      setEmailError(validate());
+    }
+  }, [email]);
 
   return (
     <Stack spacing={5}>
-      <Text fontSize='lg' color="white">Enter the email associated with your account</Text>
-      <Input type='email' textColor='antiquewhite' placeholder='Email' size='lg' />
-      <Button mt={7} colorScheme='messenger' size='lg'>Continue</Button>
-      <Center mt={3}><button onClick={goBack}><Text fontSize='lg' color="messenger.300">Go Back</Text></button></Center>
+      <Text fontSize="lg" color="white">
+        Enter the email associated with your account
+      </Text>
+      <Input
+        type="email"
+        textColor="antiquewhite"
+        placeholder="Email"
+        size="lg"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      {emailError && (
+        <Text fontSize="md" color="tomato">
+          {emailError}
+        </Text>
+      )}
+      {!isResetPasswordLink && (
+        <Button
+          mt={7}
+          colorScheme="messenger"
+          size="lg"
+          onClick={handleContinueClick}
+        >
+          Continue
+        </Button>
+      )}
+      {isResetPasswordLink && (
+        <Text fontSize="lg" color="white">
+          A password reset link has been sent to your email address
+        </Text>
+      )}
+      <Center mt={3}>
+        <Link href={"/auth/login"}>
+          <Text fontSize="lg" color="messenger.300">
+            Go Back
+          </Text>
+        </Link>
+      </Center>
     </Stack>
-  )
+  );
 }
 
-export default recoverPassword
+export default recoverPassword;
--- app/auth/reset-password/page.tsx
+++ app/auth/reset-password/page.tsx
@@ -1,25 +1,126 @@
-"use client"
+"use client";
+import React, { useEffect, useState } from "react";
+import {
+  Button,
+  Text,
+  Input,
+  Stack,
+  IconButton,
+  InputGroup,
+  InputRightElement,
+} from "../../chakraExports";
+import supabase from "../../../src/utils/supabase";
+import { useRouter } from "next/navigation";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
 
-import React from 'react'
-import { AbsoluteCenter, Box, Button, Text, Center, Divider, Input, Link, Stack } from '../../chakraExports'
-import { FcGoogle } from 'react-icons/fc'
+interface Errors {
+  [key: string]: string;
+}
 
 function resetPassword() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [fieldErrors, setFieldErrors] = useState<Errors>({});
 
-  const goBack = () => {
-    console.log("go back")
-  }
+  const validate = () => {
+    let errors: Errors = {};
+    if (!email) {
+      errors.email = "Please enter your email address";
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(email)) {
+      errors.email = "Invalid email address";
+    }
+    if (!password) {
+      errors.password = "Please enter a password";
+    } else if (!/^(?=.*[0-9])(?=.*[!@#$%^&*])(.{8,})$/i.test(password)) {
+      errors.password =
+        "Password must have minimum of 8 characters, including at least one number and one special character";
+    }
+    return errors;
+  };
+
+  const handleContinueClick = async () => {
+    const errors = validate();
+    setFieldErrors(errors);
+    if (Object.keys(errors).length === 0) {
+      try {
+        await supabase.auth.updateUser({ password: password });
+        router.push("/");
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (Object.keys(fieldErrors).length !== 0) {
+      setFieldErrors(validate());
+    }
+  }, [email, password]);
 
   return (
     <Stack spacing={5}>
-      <Text fontSize='lg' color="white">Enter the email associated with your account</Text>
-      <Input type='email' textColor='antiquewhite' placeholder='Email' size='lg' />
-      <Text fontSize='lg' color="white">Enter new password</Text>
-      <Input textColor='antiquewhite' placeholder='Password' type='password' size='lg' />
-      <Button mt={7} colorScheme='messenger' size='lg'>Continue</Button>
-      <Center mt={3}><button onClick={goBack}><Text fontSize='lg' color="messenger.300">Go Back</Text></button></Center>
+      <Text fontSize="lg" color="white">
+        Enter the email associated with your account
+      </Text>
+      {fieldErrors.email && (
+        <Text fontSize="md" color="tomato">
+          {fieldErrors.email}
+        </Text>
+      )}
+      <Input
+        type="email"
+        textColor="antiquewhite"
+        placeholder="Email"
+        size="lg"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Text fontSize="lg" color="white">
+        Enter new password
+      </Text>
+      <InputGroup>
+        <Input
+          textColor="antiquewhite"
+          placeholder="Password"
+          type={showPassword ? "text" : "password"}
+          size="lg"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <InputRightElement width="2rem">
+          <IconButton
+            h="1.5rem"
+            size="sm"
+            variant="unstyled"
+            icon={
+              showPassword ? (
+                <FaEyeSlash color="white" />
+              ) : (
+                <FaEye color="white" />
+              )
+            }
+            onClick={() => setShowPassword(!showPassword)}
+            aria-label={""}
+          />
+        </InputRightElement>
+      </InputGroup>
+      {fieldErrors.password && (
+        <Text fontSize="md" color="tomato">
+          {fieldErrors.password}
+        </Text>
+      )}
+      <Button
+        mt={7}
+        colorScheme="messenger"
+        size="lg"
+        onClick={handleContinueClick}
+      >
+        Continue
+      </Button>
     </Stack>
-  )
+  );
 }
 
-export default resetPassword
+export default resetPassword;

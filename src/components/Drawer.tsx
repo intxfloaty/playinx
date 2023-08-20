9b14390e96a25b33aaf2fc10309ed5303e20aed4
Pravesh <praveshjha5@gmail.com>
2023-08-21 01:08:40 +0530

    Initial drawer UI

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Button,
   SideMenu,
@@ -10,11 +10,38 @@ import {
   DrawerContent,
   DrawerCloseButton,
   useDisclosure,
+  Text,
+  Stack,
+  Box,
+  Flex,
 } from "../app/chakraExports";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { FaUser, FaUserFriends, FaTrophy } from "react-icons/fa";
+import { IoIosFootball, IoIosStats, IoMdLogOut } from "react-icons/io";
 
 const Drawer = () => {
+  const supabase = createClientComponentClient();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [placement, setPlacement] = React.useState("right");
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const getNameAndPhone = async () => {
+    try {
+      let { data: profiles, error } = await supabase
+        .from("profiles")
+        .select("name,phone");
+      if (profiles && error === null) {
+        setName(profiles[0].name);
+        setPhone(profiles[0].phone);
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    getNameAndPhone();
+  }, []);
 
   return (
     <>
@@ -29,13 +56,75 @@ const Drawer = () => {
         variant="primary"
       >
         <DrawerOverlay />
-        <DrawerContent>
-          <DrawerHeader borderBottomWidth="1px">Basic Drawer</DrawerHeader>
-          <DrawerBody>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
-            <p>Some contents...</p>
+        <DrawerContent padding={4}>
+          <DrawerHeader borderBottomWidth="1px" borderBottomColor="gray">
+            <Text color="#ffffff" fontSize="lg">
+              {name}
+            </Text>
+            <Text color="#ffffff" fontSize="sm">
+              {phone}
+            </Text>
+          </DrawerHeader>
+          <DrawerBody mt={5}>
+            <Stack spacing={8}>
+              <Box>
+                <Flex align="center" flexDir="row" gap={5}>
+                  <FaUser color="#ffffff" size={20} />
+                  <Text color="#ffffff" fontSize="lg">
+                    Profile
+                  </Text>
+                </Flex>
+              </Box>
+
+              <Box>
+                <Flex align="center" flexDir="row" gap={5}>
+                  <FaUserFriends color="#ffffff" size={20} />
+                  <Text color="#ffffff" fontSize="lg">
+                    My Teams
+                  </Text>
+                </Flex>
+              </Box>
+
+              <Box>
+                <Flex align="center" flexDir="row" gap={5}>
+                  <IoIosFootball color="#ffffff" size={24} />
+                  <Text color="#ffffff" fontSize="lg">
+                    Matches
+                  </Text>
+                </Flex>
+              </Box>
+
+              <Box>
+                <Flex align="center" flexDir="row" gap={5}>
+                  <FaTrophy color="#ffffff" size={20} />
+                  <Text color="#ffffff" fontSize="lg">
+                    Tournaments
+                  </Text>
+                </Flex>
+              </Box>
+
+              <Box>
+                <Flex align="center" flexDir="row" gap={5}>
+                  <IoIosStats color="#ffffff" size={24} />
+                  <Text color="#ffffff" fontSize="lg">
+                    Leaderboard
+                  </Text>
+                </Flex>
+              </Box>
+            </Stack>
           </DrawerBody>
+
+          <DrawerFooter>
+            <Box>
+              <Flex align="center" flexDir="row" gap={3}>
+                <IoMdLogOut color="#ffffff" size={24} />
+                <Text color="#ffffff" fontSize="lg">
+                  Logout
+                </Text>
+              </Flex>
+            </Box>
+
+          </DrawerFooter>
         </DrawerContent>
       </SideMenu>
     </>

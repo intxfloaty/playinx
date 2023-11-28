2641089e54127f5ae44ea980cf894df7691c62c1
Pravesh <praveshjha5@gmail.com>
2023-11-28 11:38:41 +0530

    fix not null issue

--- supabase/migrations/20231128055208_update_schema_for_players.sql
+++ supabase/migrations/20231128055208_update_schema_for_players.sql
@@ -1,9 +1,7 @@
 alter table "public"."lineup" alter column "player_id" drop default;
 
-alter table "public"."players" add column "player_user_id" uuid not null;
+alter table "public"."players" add column "player_user_id" uuid null;
 
 alter table "public"."players" add constraint "players_player_user_id_fkey" FOREIGN KEY (player_user_id) REFERENCES profiles(user_id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
 
 alter table "public"."players" validate constraint "players_player_user_id_fkey";
-
-alter table "public"."players" alter column "player_user_id" drop not null;
